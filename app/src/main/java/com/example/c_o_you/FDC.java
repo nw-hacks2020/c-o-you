d89c5467ba5d342c558e7aa6f0f4d16a6d4e0346
Napat <karns.napat@gmail.com>
2020-01-11 20:22:47 -0800

    done

---
+++ app/src/main/java/com/example/c_o_you/FDC.java
@@ -0,0 +1,53 @@
+package com.example.c_o_you;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+public class FDC {
+
+    public static void getMenuItemID(String term) throws Exception {
+        StringBuilder result = new StringBuilder();
+        String formattedTerm = term.replaceAll(" ", "%20");
+        URL url = new URL("https://api.nal.usda.gov/fdc/v1/search?api_key=uM2ndaOlQYzFPO74HfjZzoppSsnVoLL03Lw7BoAA&generalSearchInput=" + formattedTerm);
+        HttpURLConnection httpClient = (HttpURLConnection) url.openConnection();
+        httpClient.setRequestMethod("GET");
+        httpClient.setRequestProperty("Content-Type", "application/json");
+        int responseCode = httpClient.getResponseCode();
+        System.out.println("response code");
+        System.out.println(responseCode);
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpClient.getInputStream()))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(response.toString());
+            JSONArray foods = (JSONArray)jsonObject.get("foods");
+            JSONObject food = (JSONObject)foods.get(0);
+            String ingredients = (String)food.get("ingredients");
+
+            String[] ingredientsArr = ingredients.split(", ");
+            String[] topIngredientsArr = Arrays.copyOfRange(ingredientsArr, 0, 5);
+//            System.out.println("INGREDIENTSSS");
+//            System.out.println(ingredients);
+//            System.out.println(Arrays.toString(ingredientsArr));
+//            System.out.println(Arrays.toString(topIngredientsArr));
+            //print result
+            System.out.println(response.toString());
+
+        }
+    }
+}
--- app/src/main/java/com/example/c_o_you/MainActivity.java
+++ app/src/main/java/com/example/c_o_you/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.c_o_you;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.StrictMode;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("HIIII");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            //your codes here
+            try {
+                FDC.getMenuItemID("vanilla ice cream");
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
     }
 }
